aac63d6b4d6c255e04fd83a7cadc974dcd7f09d8
Daniel Ntege <danientege785@gmail.com>
2024-07-18 14:18:48 +0300

    commit

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,4 +1,6 @@
 import 'package:user_repository/src/user_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
