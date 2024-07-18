b3b8fb8824d16632bb3b6035b4fd8eef8c41c3c2
Daniel Ntege <danientege785@gmail.com>
2024-07-18 17:41:26 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app.dart';
 import 'package:flutter_app/simple_bloc_observer.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'app.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
